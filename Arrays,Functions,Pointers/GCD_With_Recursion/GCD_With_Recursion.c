702acec7c151017f1c7783edda6c52dc87c11ea0
Reethu <120480170+reethu-penumatsa@users.noreply.github.com>
2023-01-31 08:52:59 +0530

    Add files via upload

--- Arrays,Functions,Pointers/GCD_With_Recursion/GCD_With_Recursion.c
+++ Arrays,Functions,Pointers/GCD_With_Recursion/GCD_With_Recursion.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+int gcd(int,int);
+int main()
+{
+int large,small,result;
+printf("Enter the smallest and largest numbers:");
+scanf("%d %d", &small, &large);
+result=gcd(large,small);
+printf("Gcd of given two numbers is %d",result);
+return 0;
+}
+int gcd(int large,int small)
+{
+if(large<small)
+{
+return gcd(small,large);
+}
+if(small==0)
+{
+return large;
+}
+return gcd(small,large%small);
+}
 
